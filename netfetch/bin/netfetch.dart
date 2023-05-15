e87ed4b14ebf63e1748891ea23e58b8e747dfd9d
陈 豪嘉 <chenhj@mail.ynu.edu.cn>
2023-05-15 01:36:13 +0000

    netfetch

--- netfetch/bin/netfetch.dart
+++ netfetch/bin/netfetch.dart
@@ -1,5 +1,12 @@
 import 'package:netfetch/netfetch.dart' as netfetch;
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
-  print('Hello world: ${netfetch.calculate()}!');
+  Make_network_request();
+}
+
+void Make_network_request() async {
+  final httpPackageUrl = Uri.http('dart.dev', '/f/packages/http.json');
+  final httpPackageInfo = await http.read(httpPackageUrl);
+  print(httpPackageInfo);
 }
