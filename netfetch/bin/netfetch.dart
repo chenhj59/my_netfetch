77e6f90eb1bf2fd041868712f0d9d97495c8cc37
陈 豪嘉 <chenhj@mail.ynu.edu.cn>
2023-05-15 01:47:28 +0000

    netfetch

--- netfetch/bin/netfetch.dart
+++ netfetch/bin/netfetch.dart
@@ -6,7 +6,20 @@ void main(List<String> arguments) {
 }
 
 void Make_network_request() async {
-  final httpPackageUrl = Uri.http('raw.githubusercontent.com', '/chenhj59/my_netfetch/main/netfetch/bin/test.json');
+  final httpPackageUrl = Uri.http('raw.githubusercontent.com',
+      '/chenhj59/my_netfetch/main/netfetch/bin/test.json');
   final httpPackageInfo = await http.read(httpPackageUrl);
   print(httpPackageInfo);
+
+  /*The following snippet uses get to get the whole response in order to exit early if the request was not successful,
+   which is indicated with a status code of 200
+   */
+  final httpPackageResponse = await http.get(httpPackageUrl,
+  headers:{'User-Agent': '<product name>/<prodect-version>'});
+  if (httpPackageResponse.statusCode != 200) {
+    print('Failed to retrieve the http package!');
+    return;
+  }
+  print(httpPackageResponse);
+  print(httpPackageResponse.body);
 }
